40a6f63f8f6b1a669bdc4a0f6680980f6ed31a78
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2021-09-22 21:02:34 +0530

    updated changes

---
+++ model_training_app/driver_license/helper.py
@@ -0,0 +1,37 @@
+import os
+import cv2
+import random
+import numpy as np
+
+
+CLASSES = "./data/Driver_License/DL.names"
+allowes_classes = ['']
+def read_class_names(class_file_name):
+    names = {}
+    with open(class_file_name, 'r') as data:
+        for ID, name in enumerate(data):
+            names[ID] = name.strip('\n')
+    return names
+
+def crop_objects(img,data,path):
+    boxes, scores, classes, num_objects = data
+    class_names = read_class_names(CLASSES)
+    #create dictionary to hold count of objects for image name
+    counts = dict()
+    for i in range(num_objects):
+        # get count of class for part of image name
+        class_index = int(classes[i])
+        class_name = class_names[class_index]
+        if class_name in allowed_classes:
+            counts[class_name] = counts.get(class_name, 0) + 1
+            # get box coords
+            xmin, ymin, xmax, ymax = boxes[i]
+            # crop detection from image (take an additional 5 pixels around all edges)
+            cropped_img = img[int(ymin)-5:int(ymax)+5, int(xmin)-5:int(xmax)+5]
+            # construct image name and join it to path for saving crop properly
+            img_name = class_name + '_' + str(counts[class_name]) + '.png'
+            img_path = os.path.join(path, img_name )
+            # save image
+            cv2.imwrite(img_path, cropped_img)
+        else:
+            continue
