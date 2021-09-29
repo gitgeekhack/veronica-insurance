06046424b48f4d9f764fdeabe28a75678bc6d4b1
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2021-09-29 15:06:59 +0530

    Insured,Agent and Company info for 4 types

---
+++ ITC/extraction/ITC_extract_main.py
@@ -0,0 +1,71 @@
+import fitz
+from extract_type1 import ITC_Type1
+from extract_type2 import ITC_Type2
+from extract_type3 import ITC_Type3
+from extract_type4 import ITC_Type4
+
+
+path = 'D:/Office/veronica-self/ITC/new_out/out_ITC TurboRater - Breakdown[Edgar Silva].pdf.pdf'
+
+doc = fitz.open(path)
+page = doc[0]
+blocks = page.getText("dict")['blocks']
+
+length_blocks = len(blocks)
+
+
+if length_blocks>5 and length_blocks<9:
+    type1 = ITC_Type1()
+    i = type1.check_blocks(blocks)
+    print("----INSURED INFO-----")
+    Insured_info = type1.get_insured_info(blocks, i)
+    print(Insured_info)
+    print("----AGENT INFO-----")
+    Agent_info = type1.get_agent_info(blocks, i)
+    print(Agent_info)
+    print("----COMPANY INFO----")
+    Company_info = type1.get_company_info(blocks, i)
+    print(Company_info)
+    print("------------------------------------------------------------")
+elif length_blocks == 17:
+    type2 = ITC_Type2()
+    blocks = page.getText("dict")['blocks']
+    Insured_dict, Agent_dict = type2.get_personal_info(blocks)
+    print("----Insured INFO-----")
+    print(Insured_dict)
+    print("----Agent INFO----")
+    print(Agent_dict)
+    print("----Company INFO----")
+    Company_dict = type2.get_company_info(blocks)
+    print(Company_dict)
+    print("------------------------------------------------------------")
+elif length_blocks >= 11 and length_blocks<25:
+    type4 = ITC_Type4()
+    blocks = page.getText("dict")['blocks']
+    Insured_dict, Agent_dict = type4.get_personal_info(blocks)
+    print("----Insured INFO-----")
+    print(Insured_dict)
+    print("----Agent INFO----")
+    print(Agent_dict)
+    print("----Company INFO----")
+    Company_dict = type4.get_company_info(blocks)
+    print(Company_dict)
+    print("------------------------------------------------------------")
+elif length_blocks>=40:
+    type3 = ITC_Type3()
+    blocks = page.getText("dict")['blocks']
+    list = ['Name', 'Address', 'Phone Number', 'Phone', 'Phone Number ( ) -', 'Work', 'Work Number', 'Company',
+            'Policy Term', 'Quote By', 'Driver Information', 'Driver DOB', 'FR Filing',
+            'Comprehensive Deductible',
+            'Collision Deductible', 'Liability BI', 'Liability PD', 'Uninsured BI', 'Unins PD/Coll Ded',
+            'Unins PD/Coll Ded Waiver']
+    print(len(blocks))
+    block_dict = type3.check_blocks(blocks, list)
+    print("----PERSONAL INFO-----")
+    Insured_info, Agent_info = type3.get_personal_info(blocks, block_dict)
+    print(Insured_info)
+    print(Agent_info)
+    print("----Company INFO-----")
+    Company_info = type3.get_company_info(blocks, block_dict)
+    print(Company_info)
+    print("------------------------------------------------------------")
---
+++ ITC/extraction/extract_type2.py
@@ -0,0 +1,121 @@
+import fitz
+import os
+
+
+class ITC_Type2:
+    def check_blocks(self,blocks, list,s,e):
+        dict = {}
+        for i in range(s, e):
+            temp = blocks[i]['lines'][0]['spans'][0]['text'].split(' ')[0]
+            if (temp) in list:
+                dict[temp] = i
+        return dict
+
+
+    def get_personal_info(self,blocks):
+        Insured_list = ['Name', 'Address', 'City,']
+        Name = []
+        Agent_Name = []
+        Address = []
+        Insured_dict = {}
+        Agent_dict = {}
+        Zip = []
+        block_dict = self.check_blocks(blocks, Insured_list,0,7)
+        if blocks[block_dict['Name']]:
+            end = [x for x in range(1, len(blocks[block_dict['Name']]['lines'])) if
+                   blocks[block_dict['Name']]['lines'][x]['spans'][0]['text'] == 'Name']
+            for x in range(1, end[0]):
+                Name.append(blocks[block_dict['Name']]['lines'][x]['spans'][0]['text'])
+            Name = ' '.join(map(str, Name)).strip()
+            if len(blocks[block_dict['Name']]['lines']) < 8:
+                for x in range(end[0] + 1, len(blocks[block_dict['Name']]['lines'])):
+                    Agent_Name.append(blocks[block_dict['Name']]['lines'][x]['spans'][0]['text'])
+            else:
+                temp = [x for x in range(end[0] + 1, len(blocks[block_dict['Name']]['lines'])) if
+                        blocks[block_dict['Name']]['lines'][x]['spans'][0]['text'] == 'Address']
+                for x in range(end[0] + 1, temp[0]):
+                    Agent_Name.append(blocks[block_dict['Name']]['lines'][x]['spans'][0]['text'])
+                end = []
+                end = [x for x in range(temp[0], len(blocks[block_dict['Name']]['lines'])) if
+                       blocks[block_dict['Name']]['lines'][x]['spans'][0]['text'] == 'Address']
+                for x in range(end[0] + 1, end[1]):
+                    Address.append(blocks[block_dict['Name']]['lines'][x]['spans'][0]['text'])
+
+        if 'Address' in block_dict.keys():
+            end = []
+            temp = blocks[block_dict['Address']]['lines'][0]['spans'][0]['text'].split(' ')
+            if len(temp) > 1:
+                if temp[0] == 'Address':
+                    Address = temp[1:]
+            end = [x for x in range(1, len(blocks[block_dict['Address']]['lines'])) if
+                   blocks[block_dict['Address']]['lines'][x]['spans'][0]['text'] == 'Address']
+            for x in range(1, end[0]):
+                Address.append(blocks[block_dict['Address']]['lines'][x]['spans'][0]['text'])
+            end = []
+            end = [x for x in range(1, len(blocks[block_dict['Address']]['lines'])) if
+                   blocks[block_dict['Address']]['lines'][x]['spans'][0]['text'] == 'City,' or
+                   blocks[block_dict['Address']]['lines'][x]['spans'][0]['text'] == 'City, State']
+
+            if end:
+                if len(end) == 2:
+                    for x in range(end[0] + 1, end[1]):
+                        block = blocks[block_dict['Address']]
+                        line = block['lines'][x]
+                        for j, span in enumerate(line['spans']):
+                            Zip.append(span['text'])
+                    if 'State ZIP ' in Zip:
+                        Zip.remove('State ZIP ')
+                    if 'ZIP' in Zip:
+                        Zip.remove('ZIP')
+                else:
+                    for x in range(end[0], len(blocks[block_dict['Address'] + 1]['lines'])):
+                        Zip.append(blocks[block_dict['Address'] + 1]['lines'][x]['spans'][0]['text'])
+            else:
+                if blocks[block_dict['Address'] + 1]['lines'][0]['spans'][0]['text'] == 'City,':
+                    if len(blocks[block_dict['Address'] + 1]['lines']) == 2:
+                        end = []
+                        end = [x for x in range(1, len(blocks[block_dict['Address'] + 2]['lines'])) if
+                               blocks[block_dict['Address'] + 2]['lines'][x]['spans'][0]['text'] == 'State ZIP']
+
+                        for x in range(0, end[0] - 1):
+                            Zip.append(blocks[block_dict['Address'] + 2]['lines'][x]['spans'][0]['text'])
+                        if 'State ZIP ' in Zip:
+                            Zip.remove('State ZIP ')
+        Agent_list = ['Work','Phone','Work Number']
+        block_dict = self.check_blocks(blocks, Agent_list, 0, 12)
+        end = [x for x in range(1, len(blocks[block_dict['Work']]['lines'])) if
+               blocks[block_dict['Work']]['lines'][x]['spans'][0]['text'] == 'Producer' or
+               blocks[block_dict['Work']]['lines'][x]['spans'][0]['text'] == 'Producer Code']
+
+        Agent_dict['Producer Code'] = blocks[block_dict['Work']]['lines'][end[0]+2]['spans'][0]['text']
+        Address = ' '.join(map(str, Address)).strip()
+        Agent_Name = ' '.join(map(str, Agent_Name)).strip()
+
+        Insured_dict['Insured Name'] = Name
+        Insured_dict['Insured Address'] = Address
+        Agent_dict['Agent Name'] = Agent_Name
+        Insured_dict['City, State ZIP'] = ' '.join(map(str, Zip)).strip()
+        return Insured_dict, Agent_dict
+
+
+    def get_company_info(self,blocks):
+        list = [ 'Company','Policy','Rates']
+        Company_dict = {}
+        try:
+            block_dict = self.check_blocks(blocks, list, 5, 15)
+            Company_dict['Company'] = blocks[block_dict['Company']]['lines'][1]['spans'][0]['text']
+            if 'Policy' in block_dict.keys():
+                Company_dict['Policy Term'] = blocks[block_dict['Policy']]['lines'][1]['spans'][0]['text']
+            else:
+                end = [x for x in range(1, len(blocks[block_dict['Rates']]['lines'])) if
+                       blocks[block_dict['Rates']]['lines'][x]['spans'][0]['text'] == 'Policy Term']
+                Company_dict['Policy Term'] = blocks[block_dict['Rates']]['lines'][end[0]]['spans'][0]['text']
+        except:
+            block_dict = self.check_blocks(blocks, list, 5, 10)
+            Company_dict['Company'] = blocks[block_dict['Company']]['lines'][1]['spans'][0]['text']
+            end = [x for x in range(1, len(blocks[block_dict['Company']]['lines'])) if
+                   blocks[block_dict['Company']]['lines'][x]['spans'][0]['text'] == 'Policy Term']
+            Company_dict['Policy Term'] = blocks[block_dict['Company']]['lines'][end[0]+1]['spans'][0]['text']
+        return Company_dict
+
+
